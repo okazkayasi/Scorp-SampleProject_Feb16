b0fbad4983e5cf1eeb5998163339df59a33baa4f
okazkayasi <oguz.kazkayasi@saypr.com>
2022-04-09 15:17:56 +0300

    feat: start

---
+++ public/main.js
@@ -0,0 +1,19 @@
+// @ts-check
+
+import { APIWrapper, API_EVENT_TYPE } from "./api.js";
+import {
+  addMessage,
+  animateGift,
+  isPossiblyAnimatingGift,
+  isAnimatingGiftUI,
+} from "./dom_updates.js";
+
+const api = new APIWrapper();
+
+const event_queue = [];
+
+api.setEventHandler((events) => {
+  // ...
+});
+
+// NOTE: UI helper methods from `dom_updates` are already imported above.
